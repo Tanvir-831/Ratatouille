41e95de057047f9d9cd8e9917b602ce31007e15c
Tanvir-831 <tanvirhossainsaon176@gmail.com>
2024-10-22 00:25:28 +0600

    v11

--- app/src/main/java/com/example/ratatouille/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/ratatouille/Fragment/HomeFragment.java
@@ -11,10 +11,16 @@ import androidx.fragment.app.Fragment;
 
 import com.example.ratatouille.adapters.RecipeAdapter;
 import com.example.ratatouille.databinding.FragmentHomeBinding;
+import com.example.ratatouille.models.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
 private FragmentHomeBinding binding;
+List<Recipe> favouriteRecipes;
+List<Recipe> popularRecipes;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -28,8 +34,39 @@ private FragmentHomeBinding binding;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.rvFavouriteMeal.setAdapter(new RecipeAdapter());
+        loadFavouriteRecipes();
+       loadPopularRecipes();
+    }
+
+    private void loadPopularRecipes() {
+
+        popularRecipes = new ArrayList<>();
+        popularRecipes.add(new Recipe("recipe1", "recipe1", "1", "1", "1", "1", "1"));
+        popularRecipes.add(new Recipe("recipe2", "recipe2", "2", "2", "2", "2", "2"));
+        popularRecipes.add(new Recipe("3", "3", "3", "3", "3", "3", "3"));
+        popularRecipes.add(new Recipe("4", "4", "4", "4", "4", "4", "4"));
         binding.rvPopulars.setAdapter(new RecipeAdapter());
+        RecipeAdapter adapter = (RecipeAdapter) binding.rvPopulars.getAdapter();
+        if (adapter != null) {
+            adapter.setRecipeList(popularRecipes);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private void loadFavouriteRecipes() {
+        binding.rvPopulars.setAdapter(new RecipeAdapter());
+        favouriteRecipes = new ArrayList<>();
+        favouriteRecipes.add(new Recipe("recipe1","recipe1","1","1","1","1","1"));
+        favouriteRecipes.add(new Recipe("recipe2","recipe2","2","2","2","2","2"));
+        favouriteRecipes.add(new Recipe("3","3","3","3","3","3","3"));
+        favouriteRecipes.add(new Recipe("4","4","4","4","4","4","4"));
+        binding.rvFavouriteMeal.setAdapter(new RecipeAdapter());
+        RecipeAdapter adapter = (RecipeAdapter) binding.rvFavouriteMeal.getAdapter();
+        if (adapter != null) {
+            adapter.setRecipeList(favouriteRecipes);
+            adapter.notifyDataSetChanged();
+        }
+
     }
 
     @Override
--- app/src/main/java/com/example/ratatouille/adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/ratatouille/adapters/RecipeAdapter.java
@@ -1,14 +1,21 @@
 package com.example.ratatouille.adapters;
 import static com.example.ratatouille.databinding.ItemRecipeBinding.inflate;
-import android.app.Service;
+
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ratatouille.R;
 import com.example.ratatouille.databinding.ItemRecipeBinding;
+import com.example.ratatouille.models.Recipe;
+
+import java.util.List;
 
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
+
+    List<Recipe> recipeList;
 
     @NonNull
     @Override
@@ -18,12 +25,17 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
 
     @Override
     public void onBindViewHolder(@NonNull RecipeAdapter.RecipeHolder holder, int position) {
-        holder.onBind();
+        Recipe recipe = recipeList.get(position);
+        holder.onBind(recipe);
     }
 
     @Override
     public int getItemCount() {
-        return 4;
+        return recipeList.size();
+    }
+
+    public void setRecipeList(List<Recipe> favouriteRecipes) {
+            this.recipeList = favouriteRecipes;
     }
 
     public static class RecipeHolder extends RecyclerView.ViewHolder {
@@ -34,7 +46,10 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
             super(itemView.getRoot());
             binding = itemView;
         }
-        public  void onBind( ){
+        public  void onBind(Recipe recipe){
+
+            binding.bgImgRecipe.setImageResource(recipe.getImage().equalsIgnoreCase("recipe1") ? R.drawable.recipe1 : R.drawable.recipe2);
+            binding.tvRecipeName.setText(recipe.getName());
 
         }
     }
